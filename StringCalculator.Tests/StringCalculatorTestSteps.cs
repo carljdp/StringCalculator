f6f26517957667917d5caca20ddb5ffbae63492f
Carl (Callie) du Preez <cdupreez@astutefse.com>
2019-01-11 10:40:35 +0200

    step 01 test built

---
+++ StringCalculator.Tests/StringCalculatorTestSteps.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorTestSteps
+    {
+        [TestMethod]
+        public void Step01_Add_TakesAString_ReturnsANumber()
+        {
+
+            var result = StringCalculator.Add("")
+
+            Assert.AreEqual(x,x)
+
+        }
+    }
+}
